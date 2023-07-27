6d479c42b2ed69a8688826ca3ed0703a674810c1
Debasmitasutar <debasmita12.ds@gmail.com>
2023-07-27 18:10:18 +0530

    LambdaExpression Program

--- LamdaExpressionDemo.java
+++ LamdaExpressionDemo.java
@@ -9,15 +9,20 @@ public class LamdaExpressionDemo {
         IMathfunction subtract=(x,y)->x-y;
         //Division
         IMathfunction divide=(x,y)->x/y;
-        System.out.println("Addition of two number is:"+add.calculate(4,3));
-       System.out.println("Subtraction of two number is:"+subtract.calculate(4,3));
-       System.out.println("Division of two number is:"+divide.calculate(4,2));
-
+//        System.out.println("Addition of two number is:"+add.calculate(4,3));
+//       System.out.println("Subtraction of two number is:"+subtract.calculate(4,3));
+//       System.out.println("Division of two number is:"+divide.calculate(4,2));
+          IMathfunction.printResult(9,19 ,"Addition",add);
+          IMathfunction.printResult(40,11 ,"Substraction",subtract);
+          IMathfunction.printResult(50,5 ,"Division",divide);
     }
 
 }
 @FunctionalInterface
 interface IMathfunction {
     int calculate (int a,int b);
+    static void printResult(int a,int b,String function,IMathfunction fobj){
+        System.out.println("Result of:"+function+ "is"+fobj.calculate(a,b));
+    }
 
 }
